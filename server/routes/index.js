ea30b324fcd93c0ef2325263bdbfad602fd2ebeb
Kimberly Tsang <tsangkimberly14@gmail.com>
2023-12-05 17:15:30 -0500

    implemented settings

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import songRoutes from './songs.js';
 import uploadRoutes from './upload.js';
+import settingsRoutes from './settings.js';
 
 const configRoutes = (app) => {
   app.use('/songs', songRoutes);
   app.use('/upload',uploadRoutes);
+  app.use('/settings',settingsRoutes);
   app.use('*', (req, res) => {
     res.sendStatus(404);
   });
