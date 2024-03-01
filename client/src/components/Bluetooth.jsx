1cd6972f816c718550d8465c3100fc523413d1f5
Hasumi Tanemori <hasumi@tanemori.org>
2024-02-29 19:12:51 -0500

    rx and tx swap

--- client/src/components/Bluetooth.jsx
+++ client/src/components/Bluetooth.jsx
@@ -17,10 +17,10 @@ const Bluetooth = () => {
   const UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 
   // Allows the micro:bit to transmit a byte array
-  const UART_TX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  const UART_RX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
   // Allows a connected client to send a byte array
-  const UART_RX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+  const UART_TX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
   let rxCharacteristic;
 
