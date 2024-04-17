b81add65d5cb924495608b7f33c6cb9336a6d7b0
Hasumi Tanemori <hasumi@tanemori.org>
2024-04-17 10:32:34 -0400

    setting up https

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import configRoutes from './routes/index.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,16 +14,24 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000','https://teporem.github.io/HapticHero']
+  origin: ['http://localhost:3000','https://teporem.github.io', 'https://www.haptichero.site', 'https://haptichero.site']
 }
 ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+const options = {
+  cert: readFileSync('/etc/letsencrypt/live/server.haptichero.site/fullchain.pem'),
+  key: readFileSync('/etc/letsencrypt/live/server.haptichero.site/privkey.pem')
+}
+
 configRoutes(app);
 
-app.listen(3001, () => {
+https.createServer(options, app).listen(443);
+console.log('Your server will be running on 443');
+/*
+app.listen(443, () => {
     console.log("We've now got a server!");
     console.log('Your routes will be running on http://localhost:3001');
-});
+});*/
